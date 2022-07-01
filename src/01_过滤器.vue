0625ec67e2deea6008c7ca7e6cca6b6a86343388
renbuairen <1664844124@qq.com>
2022-07-01 19:40:42 +0800

    1.过滤器

---
+++ src/01_过滤器.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ msg | toUp }}
+    <br />
+    <h2 v-bind:title="msg | toUp">{{ msg | toReverse('*') }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello, world!',
+    }
+  },
+  filters: {
+    toUp(val) {
+      return val.toUpperCase()
+    },
+  },
+}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.filter('toDown', val => val.toLowerCase())
+Vue.filter('toReverse', (val, split) => val.split('').reverse().join(split || ''))
 
 new Vue({
   render: h => h(App),
