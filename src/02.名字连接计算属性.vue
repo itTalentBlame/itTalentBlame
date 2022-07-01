23d076ed6902554b2c1a7c6e8c3e6c7ef6b70d00
renbuairen <1664844124@qq.com>
2022-07-01 19:52:06 +0800

    2.名字连接是计算属性

---
+++ src/02.名字连接计算属性.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <span>姓:</span>
+      <input type="text" v-model="firsName" />
+      <br />
+      <span>名:</span>
+      <input type="text" v-model="secName" />
+      <br />
+      <span>全称:</span>
+      <input type="text" v-model="fullName" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firsName: '',
+      secName: '',
+    }
+  },
+  computed: {
+    fullName: {
+      get() {
+        return this.firsName ? this.firsName + `.` + this.secName : ''
+      },
+      set(val) {
+        const arr = val.split('.')
+        this.firsName = arr[0] || ''
+        this.secName = arr[1] || ''
+      },
+    },
+  },
+}
+</script>
+
+<style></style>
