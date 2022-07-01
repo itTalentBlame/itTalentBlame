2f6a740c85764495481b7cfd445552fa00cc938d
bianling <1343881168@qq.com>
2022-07-01 19:07:59 +0800

    完成了全选反选案例

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,55 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <!-- 顶部框模块 -->
-      <div class="form-group">
-        <div class="input-group">
-          <h4>品牌管理</h4>
-        </div>
-      </div>
-
-      <!-- 数据表格 -->
-      <table class="table table-bordered table-hover mt-2">
-        <thead>
-          <tr>
-            <th>编号</th>
-            <th>资产名称</th>
-            <th>价格</th>
-            <th>创建时间</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in list" :key="item.id">
-            <td>{{ item.id }}</td>
-            <td>{{ item.name }}</td>
-            <td :class="{ red: item.price > 100 }">{{ item.price }}</td>
-            <td>{{ item.time }}</td>
-            <td><a href="#" @click="del(item.id)">删除</a></td>
-          </tr>
-        </tbody>
-        <!-- 统计总价格 -->
-        <tr style="background-color: #eee">
-          <td>统计:</td>
-          <td colspan="2">总价钱为: {{ allPrice }}</td>
-          <td colspan="2">平均价: {{ svgPrice }}</td>
-        </tr>
-      </table>
-
-      <!-- 添加资产 -->
-      <form class="form-inline" style="display: flex">
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="资产名称"
-              v-model.trim="name"
-            />
-          </div>
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              type="number"
-              class="form-control"
-              placeholder="价格"
-              v-model.trim="price"
-            />
-          </div>
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <!-- 阻止表单提交 -->
-        <button class="btn btn-primary" @click.prevent="addFn">添加资产</button>
-      </form>
-    </div>
+  <div>
+    <span>全选:</span>
+    <input type="checkbox" v-model="isAll" />
+    <button @click="fn">反选</button>
+    <ul>
+      <li v-for="item in arr" :key="item.name">
+        <input type="checkbox" v-model="item.c" />
+        <span>{{ item.name }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// 1. 明确需求
-// 2. 标签+样式+默认数据
-// 3. 下载bootstrap, main.js引入bootstrap.css
-// 4. 把list数组 - 铺设表格
-// 5. 修改价格颜色
 export default {
   data() {
     return {
-      list: [
-        { id: 100, name: "外套", price: 199, time: new Date("2010-08-12") },
-        { id: 101, name: "裤子", price: 34, time: new Date("2013-09-01") },
-        { id: 102, name: "鞋", price: 25.4, time: new Date("2018-11-22") },
-        { id: 103, name: "头发", price: 19900, time: new Date("2020-12-12") },
+      arr: [
+        {
+          name: "猪八戒",
+          c: false,
+        },
+        {
+          name: "孙悟空",
+          c: false,
+        },
+        {
+          name: "唐僧",
+          c: false,
+        },
+        {
+          name: "白龙马",
+          c: false,
+        },
       ],
-      name: "",
-      price: "",
     };
   },
-  methods: {
-    addFn() {
-      if (!this.name || !this.price) return alert("内容不能为空");
-      this.list.push({
-        id: this.list[0] ? this.list[this.list.length - 1].id + 1 : 100,
-        name: this.name,
-        price: this.price,
-        time: new Date(),
-      });
-      this.name = "";
-      this.price = "";
-    },
-    del(id) {
-      let index = this.list.findIndex((item) => id == item.id);
-      this.list.splice(index, 1);
+  computed: {
+    isAll: {
+      get() {
+        return this.arr.every((item) => item.c);
+      },
+      set(val) {
+        this.arr.forEach((ele) => (ele.c = val));
+      },
     },
   },
-  computed: {
-    allPrice() {
-      return this.list.reduce((req, next) => (req += +next.price), 0).toFixed(2)
+  methods: {
+    fn() {
+      this.arr.forEach((item) => (item.c = !item.c));
     },
-    svgPrice(){
-      return (this.allPrice/this.list.length).toFixed(2)
-    }
   },
 };
 </script>
-
-<style >
-.red {
-  color: red;
-}
-</style>
