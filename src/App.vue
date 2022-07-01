883efbb0e15628a88d9f27a83661ecbaba373c8d
bianling <1343881168@qq.com>
2022-07-01 18:00:20 +0800

    filters连写

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <span>{{ sum | reverse | toUp }}</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      sum: "hello word",
+    };
+  },
+  filters: {
+    reverse(val) {
+      return val.split("").reverse().join("");
+    },
+    toUp(val) {
+      return val.toUpperCase();
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
