5dd019c4e808e4a49b6cd6aaac719a054128fe6a
xx0tt <307457823@qq.com>
2022-07-01 21:30:51 +0800

    优化了过滤数量

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,18 @@
   <div>
     <h2>用同一个过滤器处理粉丝数量转换</h2>
 
-    <h4>国内网站：{{ 1876986 | person }}</h4>
+    <h4>国内网站：{{ 1876986 | person(1) }}</h4>
 
-    <h4>国外网站：{{ 1876986 | person(3) }}</h4>
+    <h4>国外网站：{{ 1876986 | person(0) }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   filters: {
-    person(val, tpl = 4) {
-      const arr = (val + '').split('')
-      const lastnum = arr.slice(arr.length - tpl).join('')
-      const firstnum = arr.slice(0, arr.length - tpl).join('')
-      return ` ${firstnum}.${lastnum}${tpl == 4 ? '万' : '千'} `
+    person(val, flag) {
+      if (flag) return (val / 10000).toFixed(4) + '万'
+      return (val / 1000).toFixed(3) + '千'
     },
   },
 }
