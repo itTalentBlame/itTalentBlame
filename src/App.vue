b291f74eabde2e5a3c7117fec0a0b3ab1d780780
bianling <1343881168@qq.com>
2022-07-01 20:26:22 +0800

    完成了过滤器

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,58 @@
 <template>
-    <div>
-      用户名: <input type="text" v-model="user.username"><br>
-      密码:<input type="text" v-model="user.password">
-    </div>
+  <div>
+    <div>国内粉丝{{ fans | chinaFans(1) }}</div>
+    <div>国外粉丝{{ fans | chinaFans(0) }}</div>
+    <!-- 处理时间戳 -->
+    <div>{{ timer | timerFn("YYYY-MM-DD") }}</div>
+    <div>{{ timer | timerFn("YYYY/MM/DD") }}</div>
+  </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
-    data(){
-      return{
-        user:{
-          username:'',
-          password:''
-        }
+  data() {
+    return {
+      fans: "16876986",
+      timer: 1636450540055,
+    };
+  },
+  filters: {
+    chinaFans(val, flag) {
+      if (flag) {
+        return (
+          val
+            .split("")
+            .splice(0, val.length - 4)
+            .join("") +
+          "." +
+          val
+            .split("")
+            .splice(val.length - 4)
+            .join("") +
+          "万"
+        );
+      } else {
+        return (
+          val
+            .split("")
+            .splice(0, val.length - 3)
+            .join("") +
+          "." +
+          val
+            .split("")
+            .splice(val.length - 3)
+            .join("") +
+          "千"
+        );
       }
     },
-    watch:{
-      user:{
-        handler(newval){
-          console.log(newval);
-        },
-        //深侦听
-        deep:true,
-        //立即侦听
-        immediate:true
-      }
-    }
-}
+    timerFn(val,end) {
+      return moment(val).format(end);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
