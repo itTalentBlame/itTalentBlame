6291696936680ff98c9c344ccaf5fe7f589bf0f8
bianling <1343881168@qq.com>
2022-07-01 18:05:42 +0800

    完成了格式化时间

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <span>{{ sum | reverse | toUp }}</span>
+    <div>{{ date | momentTime }}</div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   data() {
     return {
       sum: "hello word",
+      date: new Date(),
     };
   },
   filters: {
@@ -17,6 +20,10 @@ export default {
     },
     toUp(val) {
       return val.toUpperCase();
+    },
+    //格式化时间
+    momentTime(val) {
+      return moment(val).format("YYYY-MM-DD");
     },
   },
 };
