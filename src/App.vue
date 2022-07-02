5cb9c8eb8343c1086ea8784d70fe0fa43dd10bd9
bianling <1343881168@qq.com>
2022-07-02 13:01:35 +0800

    完成了购物车案例

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,176 @@
 <template>
-  <div>
-    <div>国内粉丝{{ fans | chinaFans(3) }}</div>
-    <div>国外粉丝{{ fans | chinaFans(4) }}</div>
-    <!-- 处理时间戳 -->
-    <div>{{ timer | timerFn("YYYY-MM-DD") }}</div>
-    <div>{{ timer | timerFn("YYYY/MM/DD") }}</div>
+  <div id="app">
+    <table class="tb">
+      <tr>
+        <th><input type="checkbox" v-model="isAll" />全选</th>
+        <th>商品</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>小记</th>
+        <th>操作</th>
+      </tr>
+      <!-- 循环渲染的元素tr -->
+      <tr v-for="item in list" :key="item.id">
+        <td><input type="checkbox" v-model="item.buy" /></td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.money }}</td>
+        <td>
+          <button @click="reduce(item.id)">-</button>
+          <input type="number" v-model="item.num" />
+          <button @click="add(item.id)">+</button>
+        </td>
+        <td>{{ item.money * item.num }}</td>
+        <td><button @click="del(item.id)">删除</button></td>
+      </tr>
+
+      <tr v-if="list.length === 0">
+        <td colspan="4">没有数据咯~</td>
+      </tr>
+    </table>
+    <br />
+    <button @click="delChecked">删除选中商品</button>
+    <button @click="delAll">清理购物车</button>
+    <br />
+    <div style="margin-top: 20px">
+      <h2>统计</h2>
+      <p>已经选中商品件数:{{ quantity }}</p>
+      <p>总价:{{ moneyAll }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import moment from "moment";
 export default {
   data() {
     return {
-      fans: 16876986,
-      timer: 1636450540055,
+      list: [
+        {
+          id: 1,
+          name: "奔驰",
+          time: "2020-08-01",
+          money: 200,
+          num: 0,
+          buy: false,
+        },
+        {
+          id: 2,
+          name: "宝马",
+          time: "2020-08-02",
+          money: 300,
+          num: 0,
+          buy: false,
+        },
+        {
+          id: 3,
+          name: "奥迪",
+          time: "2020-08-03",
+          money: 400,
+          num: 0,
+          buy: false,
+        },
+      ],
     };
   },
-  filters: {
-    chinaFans(val, index) {
-      return (
-        val / Math.pow(10, index).toFixed(index) + `${index == 3 ? "千" : "万"}`
-      );
+  methods: {
+    //减
+    reduce(id) {
+      const index = this.list.findIndex((item) => item.id == id);
+      this.list[index].num--;
+      // if (this.list[index].num <= 0) this.list[index].num = 0;
     },
-    timerFn(val, end) {
-      return moment(val).format(end);
+    //加
+    add(id) {
+      const index = this.list.findIndex((item) => item.id == id);
+      this.list[index].num++;
+    },
+    // 删除
+    del(id) {
+      const index = this.list.findIndex((item) => item.id == id);
+      this.list.splice(index, 1);
+    },
+    // 删除所有
+    delAll() {
+      this.list = [];
+    },
+    // 删除选中
+    delChecked() {
+      this.list = this.list.filter((item) => !item.buy);
+    },
+  },
+  computed: {
+    //全选
+    isAll: {
+      set(val) {
+        this.list.forEach((item) => (item.buy = val));
+      },
+      get() {
+        if (this.list.length === 0) return false;
+        return this.list.every((item) => {
+          return item.buy;
+        });
+      },
+    },
+    //统计选中商品的件数
+    quantity() {
+      //选出勾选了的商品
+      const checkedBuy = this.list.filter((item) => item.buy);
+      //将勾选商品的商品
+      return checkedBuy.reduce((item, next) => {
+        return item + next.num;
+      }, 0);
+    },
+    // 选中的价格
+    moneyAll() {
+      const checkedBuy = this.list.filter((item) => item.buy);
+      return checkedBuy.reduce((item, next) => {
+        return item + next.num * next.money;
+      }, 0);
+    },
+  },
+  watch: {
+    list: {
+      handler() {
+        this.list.forEach((item) => {
+          if (item.num < 0) {
+            item.num = 0;
+          }
+        });
+      },
+      deep: true,
     },
   },
 };
 </script>
 
 <style>
+#app {
+  width: 600px;
+  margin: 10px auto;
+}
+
+.tb {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.tb th {
+  background-color: #0094ff;
+  color: white;
+}
+
+.tb td,
+.tb th {
+  padding: 5px;
+  border: 1px solid black;
+  text-align: center;
+}
+
+.add {
+  padding: 5px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+input[type="number"] {
+  -moz-appearance: textfield !important;
+}
 </style>
